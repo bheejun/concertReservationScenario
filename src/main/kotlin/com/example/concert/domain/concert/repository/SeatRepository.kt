3dccabbe2b13798348ef035aa51bf10d6efb83df
bheejun <bhjun95@gmail.com>
2024-02-07 10:04:34 +0900

    [FEAT] 쿼리문을 통한 비관적 락 적용
    예약 요청이 들어 왔을 때, 동시성 제어가 필요한 엔티티인 Seat 에 비관적 락을 걸어 동시성 제어 하도록 기능 추가

--- src/main/kotlin/com/example/concert/domain/concert/repository/SeatRepository.kt
+++ src/main/kotlin/com/example/concert/domain/concert/repository/SeatRepository.kt
@@ -1,8 +1,17 @@
 package com.example.concert.domain.concert.repository
 
 import com.example.concert.domain.concert.model.Seat
+import com.example.concert.domain.reservation.model.Reservation
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
 interface SeatRepository : JpaRepository<Seat, UUID> {
+    fun findAllByReservation(reservation : Reservation) : MutableList<Seat>
+    fun findAllByScheduleId(scheduleId : UUID) : List<Seat>
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Seat s where s.id in :seatIdList")
+    fun findByIdWithPessimisticLock(seatIdList: List<UUID>) : List<Seat>
 }
