3edaf2206b07c7930b0dab609ae7369893b4f92f
bheejun <bhjun95@gmail.com>
2024-02-16 17:33:18 +0900

    [DELETE] 사용되지 않는 클래스 삭제

--- src/main/kotlin/com/example/concert/util/event/ReservationCompleteEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.example.concert.util.event
-
-import org.springframework.context.ApplicationEvent
-import java.util.UUID
-
-class ReservationCompleteEvent(source : Any, val memberId: UUID, val scheduleId: UUID) :ApplicationEvent(source)
