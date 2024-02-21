49e9f9906e1347689bb5573e1f378b036d0d60bc
bheejun <bhjun95@gmail.com>
2024-02-21 10:56:46 +0900

    [CHORE] cicd test

--- src/main/kotlin/com/example/concert/domain/payment/PaymentController.kt
+++ src/main/kotlin/com/example/concert/domain/payment/PaymentController.kt
@@ -65,7 +65,6 @@ class PaymentController(
 
         }catch (e: RuntimeException){
             paymentService.refundForExpiredReservation(apiKey, secretKey, reservationId)
-
             throw e
         }
 
