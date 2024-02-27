1b1994152f1732b0d248d2d63e585ca09da4c0d3
bheejun <bhjun95@gmail.com>
2024-02-28 00:27:21 +0900

    [TEST] CICD test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,14 @@
 # Secret Manager에서 설정 값을 가져옵니다.
 application_yml_content=$(gcloud secrets versions access 1 --secret="concert-application-yml")
 
-
+# Secret retrieval validation
+if [ -z "$application_yml_content" ]; then
+    echo "Failed to retrieve application.yml content from Secret Manager"
+    exit 1
+fi
 
 # 설정 값을 파일로 저장합니다.
 echo "$application_yml_content" > /app/config/application.yml
 
-# 원래의 애플리케이션 실행 명령어를 실행합니다.
-exec java -jar /app/concert-0.0.1-SNAPSHOT.jar
+# 원래의 애플리케이션 실행 명령어를 실행합니다, 위치를 지정합니다.
+exec java -jar /app/concert-0.0.1-SNAPSHOT.jar --spring.config.location=file:/app/config/application.yml
